1f873608dd9766ef6c00954b5582937beb348f53
Oswald Zink <oswald@znk.dev>
2026-06-18 21:19:16 +0200

    Set macOS key repeat speed

--- macos/defaults/key-repeat.sh
+++ macos/defaults/key-repeat.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Disabling press-and-hold for keys..."
+echo "Configuring key repeat..."
 
 defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
